5aedf2cfac7632059c15f28fbcfcad677d35d139
Gedeón Domínguez <gedeondominguez@MacBook-Pro-de-Gedeon.local>
2019-01-07 21:41:29 +0100

    Login básico

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
+import { Grommet } from 'grommet';
 import Content from './components/Content';
+import firebase from "firebase";
 
 
 class App extends Component {
+
+  app = firebase.initializeApp({
+    apiKey: "AIzaSyAchTIkNwIjoi3digb_HH1wxp8MEa38VfA",
+    authDomain: "lamparas11.firebaseapp.com",
+    databaseURL: "https://lamparas11.firebaseio.com",
+    projectId: "lamparas11",
+    storageBucket: "lamparas11.appspot.com",
+    messagingSenderId: "344068527809"
+  });
+
+  state = { loading: true, authenticated: false, user: null }
+
+  componentWillMount() {
+    this.app.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          currentUser: user,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          currentUser: null,
+          loading: false
+        });
+      }
+    });
+  }
+
+  login(user, password) {
+    console.log(user, password)
+    this.app.auth().signInWithEmailAndPassword(user, password)
+  }
+
   render() {
     return (
-      <div>
-        <Navbar />
-        <br/>
-        <Content />
-      </div>
+      <Grommet>
+        { !this.state.authenticated && <Content login={ (user,password) => this.login(user, password) }/> } 
+      </Grommet>
     );
   }
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { Box, Button, FormField, TextInput } from 'grommet';
 
 export default class Content extends Component {
+
+    state = { user: '', password: '' }
+
+  onSubmit(event) {
+    event.preventDefault()
+    this.props.login(this.state.user, this.state.password)
+  }
+  
   render() {
+
+    const { user, password } = this.state;
+
     return (
-      <div className="card">
-         <div className="card-body">
-         <h5 className="card-title">React PWA Example</h5>
-         <p className="card-text">A Progressive Web App (PWA) is a web app that uses advanced web capabilities to provide an app-like experience to users. Unlike traditional applications, Progressive web apps are a hybrid of regular web pages (or websites) and a mobile application.</p>
-         <a href="https://appdividend.com" className="btn btn-primary">Go somewhere</a>
-         </div>
-      </div>
+    <form onSubmit={event => this.onSubmit(event) }>
+      <Box margin="large" align="center">
+        <FormField label='Usuario'>
+          <TextInput value={user} onChange={event => this.setState({ user: event.target.value })} />
+        </FormField>
+        <FormField label='Contraseña'>
+          <TextInput value={password} onChange={event => this.setState({ password: event.target.value })} />
+        </FormField>
+        <Button type='submit' label='Entrar' primary={true} />
+      </Box>
+    </form>
     )
    }
 }
